82dcc25e82e9e78496198fe40764fae199263612
BEATRIZ MARIA PEREIRA MAIA <beatrizmaia0544@digitalcollege.internal>
2025-06-10 21:10:50 -0300

    conceito de objeto

---
+++ objeto.js
@@ -0,0 +1,33 @@
+// 1. Criando um objeto
+
+let pessoa = {
+    nome: 'Ana',
+    cpf: '00000000000',
+    genero: 'F', 
+    altura: 1.65,
+    dataNascimento: '25/06/1993',
+    endereco: 'Rua 01, Casa 02, Messejana, Fortaleza, Ceará',
+
+    adicionar: function () {
+        console.log('Adicionando ...')
+    }, 
+    
+    remover: function () {
+        console.log('Removendo...')
+    },
+
+    estrangero: false,
+    
+    contato: {
+        telefone: '999999999',
+        email: 'ana@ig.com.br',
+    }
+
+}
+
+//2. Acessando os dados do objeto (objeto.atributo)
+console.log(pessoa.dataNascimento);
+console.log(pessoa.endereco);
+console.log(pessoa.contato.email);
+
+
